cc511630633532827322ecc9de44ca037c57793d
ArturTkachov <arturtk.tkachov@gmail.com>
2022-03-24 18:52:13 +0300

    add swap currencies button

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import Dropdown from 'components/Dropdown';
 import CurrencyInput from 'components/CurrencyInput';
 import useCurrencyRatios from 'hooks/useCurrencyRatios';
 import { getProductToFixed, getQuotientToFixed } from 'utility/multiplication';
+import PinkButton from './components/PinkButton';
 import { Currencies, Currency, LowercaseCurrency } from 'types/Currency';
-import './App.css';
+import './App.scss';
 
 function App() {
   const [from, setFrom] = useState<Currency>('USD');
@@ -12,7 +13,7 @@ function App() {
   const currencies: Currencies = ['USD', 'BYN', 'RUB'];
 
   const [fromValue, setFromValue] = useState('');
-  const [toValue, seToValue] = useState('');
+  const [toValue, setToValue] = useState('');
 
   const { data } = useCurrencyRatios(from);
   let ratio = data
@@ -25,41 +26,59 @@ function App() {
     const value = parseFloat(e.target.value);
     if (isNaN(value)) return;
     setFromValue(String(value));
-    seToValue(getProductToFixed(value, ratio));
+    setToValue(getProductToFixed(value, ratio));
   };
 
   const handleToValueChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     const value = parseFloat(e.target.value);
     if (isNaN(value)) return;
     setFromValue(getQuotientToFixed(value, ratio));
-    seToValue(String(value));
+    setToValue(String(value));
   };
 
+  const [isSwapped, setIsSwapped] = useState(false);
+  const handleSwap = () => {
+    if (isSwapped) {
+      setFromValue(getProductToFixed(+fromValue, ratio));
+      setToValue(getProductToFixed(+toValue, ratio));
+    } else {
+      setFromValue(getQuotientToFixed(+fromValue, ratio));
+      setToValue(getQuotientToFixed(+toValue, ratio));
+    }
+    setIsSwapped(!isSwapped);
+  };
   return (
-    <>
-      <Dropdown
-        labelText="From"
-        options={currencies}
-        currentOptionIndex={currencies.indexOf(from)}
-        setOption={setFrom}
-      />
-      <CurrencyInput
-        currency={from}
-        value={fromValue}
-        handleChange={handleFromValueChange}
-      />
-      <Dropdown
-        labelText="To"
-        options={currencies}
-        currentOptionIndex={currencies.indexOf(to)}
-        setOption={setTo}
-      />
-      <CurrencyInput
-        currency={to}
-        value={toValue}
-        handleChange={handleToValueChange}
-      />
-    </>
+    <main>
+      <div id="currency-panels" className={isSwapped ? 'swapped' : ''}>
+        <div>
+          <Dropdown
+            labelText={isSwapped ? 'To' : 'From'}
+            options={currencies}
+            currentOptionIndex={currencies.indexOf(from)}
+            setOption={setFrom}
+          />
+          <CurrencyInput
+            currency={from}
+            value={fromValue}
+            handleChange={handleFromValueChange}
+          />
+        </div>
+        <div>
+          <Dropdown
+            labelText={isSwapped ? 'From' : 'To'}
+            options={currencies}
+            currentOptionIndex={currencies.indexOf(to)}
+            setOption={setTo}
+          />
+          <CurrencyInput
+            currency={to}
+            value={toValue}
+            handleChange={handleToValueChange}
+          />
+        </div>
+      </div>
+      <PinkButton text="Swap" handleClick={handleSwap} />
+    </main>
   );
 }
 
